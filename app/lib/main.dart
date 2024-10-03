30bd4a9ca27e93d1216e8dd8d89fdb82acf4997d
tidalcurrent <sidneysteward@gmail.com>
2024-10-03 18:45:47 -0400

    addborder

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -11,35 +11,51 @@ class MyApp extends StatelessWidget {
   }
 }
 class FadingTextAnimation extends StatefulWidget {
-@override
-_FadingTextAnimationState createState() => _FadingTextAnimationState();
+  @override
+  _FadingTextAnimationState createState() => _FadingTextAnimationState();
 }
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
-bool _isVisible = true;
-void toggleVisibility() {
-  setState(() {
-    _isVisible = !_isVisible;
-  });
-}
+  bool _isVisible = true;
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+  bool _isFrameVisible = true;
+  void toggleFrameVisibility(){
+    setState(() {
+      _isFrameVisible = !_isFrameVisible;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      title: Text('Fading Text Animation'),
+        title: Text('Fading Text Animation'),
       ),
-        body: Center(
-          child: AnimatedOpacity(
-            opacity: _isVisible ? 1.0 : 0.0,
-            duration: Duration(seconds: 1),
-            child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child:
+          Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              border: Border.all(
+                color: Colors.deepPurpleAccent,
+                width: 2,
+              ),
+              image: DecorationImage(
+                  image: AssetImage("assets/images/miku.png"),
+              ),
             ),
           ),
         ),
+      ),
       floatingActionButton: FloatingActionButton(
-      onPressed: toggleVisibility,
-      child: Icon(Icons.play_arrow),
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
       ),
     );
   }
